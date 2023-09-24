d25752148f28da3420e5ed250889cb46f785c8d5
MaximilianWebb <max.t.webb@gmail.com>
2023-09-24 17:26:50 -0600

    Add Icon And Current Date

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image,} from 'react-native';
+import moment from 'moment/moment';
+
 
 export default function App() {
+  
+
+  var date = moment().utcOffset('+5:30').format('YYYY-MM-DD hh:mm:ss a');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style ={textStyles}> Welcome to the MoHi Schedule App!</Text>
+
+      
+      <Text style = {textStyles}> Today's Date: {date} </Text>
+     
+      <Image style = {styles.image} source={require("./assets/info.png")} />
       <StatusBar style="auto" />
     </View>
   );
+
 }
+
+const textStyles = {color : '#FFD700'};
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +30,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  setColor:{
+    color: '#FFD700',
+  },
+  image:{
+    width : 50, 
+    height: 50,
+    position: 'absolute',
+      left: 20,
+      top: 50,
+      bottom: 0,
+      right: 0
+  }
 });
