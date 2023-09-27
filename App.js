ba95decab0d38304b34e0eb1c715f3a7af555f5e
MaximilianWebb <max.t.webb@gmail.com>
2023-09-26 20:47:49 -0600

    Add Period.js and Instantiate Period in App.js

--- App.js
+++ App.js
@@ -4,14 +4,11 @@ import moment from 'moment/moment';
 import {NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import * as React from 'react';
-
+import Period from './Period';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  
-
- 
 
   return (
 
@@ -33,15 +30,24 @@ export default function App() {
 }
 
 const Home = ({navigation}) => {
+
   var date = moment().utcOffset('+5:30').format('YYYY-MM-DD hh:mm:ss a');
+
+  var test = new Period([8,35],[9,27]);
+  
+  var test1 = test.getArray();
+
   return (
     <View style={styles.container}>
       <Text style ={textStyles}> Welcome to the MoHi Schedule App!</Text>
 
-      
+      <Text style = {textStyles}> Period 1 Time: {test1} </Text>
+
+
       <Text style = {textStyles}> Today's Date: {date} </Text>
 
       <Pressable onPress={() => navigation.navigate('Info', {name: 'Info'})}>
+
       <Image style = {styles.image} source={require("./assets/info.png")}  />
       </Pressable>
       <StatusBar style="auto" />
---
+++ Period.js
@@ -0,0 +1,24 @@
+import {Text} from 'react-native';
+
+export default class Period{
+    
+    start;
+    end;
+
+    constructor(s,e){
+        start = [s];
+        end = [e];
+    }
+
+    getArray(){
+        return <Text> {start} {end} </Text>;
+    }
+
+    setStart(s){
+        start = [s];
+    }
+    setEnd(e){
+        end = [e];    
+    }
+
+}
