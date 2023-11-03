6cc8c56c35e7f72234f8c31a03096efe1acbbe10
socho1 <joshmyeongkim@gmail.com>
2023-11-03 11:01:31 -0600

    completed homescreen, added function to track time in data

--- Data.js
+++ Data.js
@@ -1,49 +1,94 @@
-import {Text, View} from 'react-native';
-
+import {Text, View, StyleSheet} from 'react-native';
+import moment from 'moment/moment';
 export const Period = function (periodNum, sTime, eTime) {
-    const periodNumber = periodNum
-    const startTime = sTime
-    const endTime = eTime
-    return{periodNumber, startTime, endTime}
+  const splitStart = sTime.split(":");
+  const splitEnd = eTime.split(":");
+  //fixes abnormal case for when hour changes from 12 to 1
+  if(splitStart[0] == 12)
+  {splitStart[0] = splitStart[0] - 12}
+  if(splitStart[0] == 12 && splitEnd[0] == 12)
+  {splitEnd[0] = splitEnd[0] - 12}  
+  //end and start are two Numbers that represent start and end time in minutes
+  const end = splitEnd[0] * 60 + Number(splitEnd[1])
+  const start = splitStart[0] * 60 + Number(splitStart[1])
+  //stashes the current hour and minute 
+    let hours = new Date().getHours(); 
+    const min = new Date().getMinutes(); 
+  //changes hours to standard formatting, however, if it is after 8pm it keeps it the same
+    if(hours > 12 && hours < 20)
+    {hours = hours - 12}
+    const timeMins = hours * 60 + min;
+    if(timeMins >= start && timeMins <= end)
+    {currentPeriod = true}
+    else
+    {currentPeriod = false}
+    
+    const periodNumber = periodNum;
+    const startTime = sTime;
+    const endTime = eTime;
+
+
+    return{currentPeriod, periodNumber, startTime, endTime}
 }
 
-export const Schedule = function () {
-
-}
 
 export const PeriodBox = function (props) {
     return(
-    <View style = {periodBox}>
-        <Text style = {headerText}>
+    <View 
+        style = {props.isPeriod
+        ? [styles.periodBox, styles.outlineBox]
+        : styles.periodBox
+        }>
+        <Text style = {styles.headerText}>
         {props.period} Period {'\n'} 
         </Text>
-        <Text style = {descriptorText}>
+        <Text style = {styles.descriptorText}>
          {props.start} - {props.end}  
         </Text>
     </View>
     )
    
 }
-
-const descriptorText = {
-        bottom: 15,
-        color: '#878787',
-        fontSize: 20,
-        fontFamily: 'Arial',
-        textAlign: 'center',
+export const currentDay = function () {
+  const currentDay = new Date().getDay();
+  let day = "";
+  if(currentDay == 1)
+  {day = "Monday"}
+  if(currentDay == 2)
+  {day = "Tuesday"}
+  if(currentDay == 3)
+  {day = "Wednesday"}
+  if(currentDay == 4)
+  {day = "Thursday"}
+  if(currentDay == 5)
+  {day = "Friday"}
+  if(currentDay == 6)
+  {day = "Saturday"}
+  if(currentDay == 7)
+  {day = "Sunday"}
+  return(day)
 }
-const headerText= {
+const styles = StyleSheet.create({
+ descriptorText: {
+        color: '#B2B2B2',
+        fontSize: 14,
+        fontFamily: 'Arial',
+        left: 15,
+        bottom: 7,
+},
+ headerText: {
       top: 7,
       color: '#FFFFFF',
-      fontSize: 25,
+      fontSize: 16,
       fontFamily: 'Arial',
-      textAlign: 'center',
-
-    };
-const periodBox = {
+      left: 15,
+    },
+ periodBox: {
+        top: 70,
         backgroundColor: '#222222',
         marginBottom: 12,
         width: 370,
+        height: 70,
         shadowOffset: { height: 4, width: 0 },
         shadowOpacity: 1,
         shadowRadius: 4,
@@ -52,4 +97,11 @@ const periodBox = {
         justifyContent: 'center',
         marginLeft: 10,
         marginRight: 10, 
-      };
+      },
+outlineBox: {
+        width: 375,
+        height: 70,
+        borderWidth: 3,
+        borderColor: '#D4AF37', 
+      },
+    })
--- Home.js
+++ Home.js
@@ -1,36 +1,82 @@
-import {Period, PeriodBox} from './Data'
-import { StyleSheet, Text, View ,Image, Pressable,} from 'react-native';
+import {Period, PeriodBox, currentDay} from './Data'
+import moment from 'moment/moment';
+import { ScrollView, StyleSheet, Text, View ,Image, Pressable,} from 'react-native';
 
 export const Home = () => {
     //hardcoding all 7 period variables
-  const per1 = Period("1st", " 8:35", "9:27");
-  const per2 = Period("2nd", " 9:32", "10:24");
+  const per1 = Period("1st", "8:35", "9:27");
+  const per2 = Period("2nd", "9:32", "10:24");
   const per3 = Period("3rd", "10:29", "11:27");
   const per4 = Period("4th", "12:12", "1:04");
-  const per5 = Period("5th", " 1:09", "2:01");
-  const per6 = Period("6th", " 2:06", "2:58");
-  const per7 = Period("7th", " 3:03", "3:55");
+  const per5 = Period("5th", "1:09", "2:01");
+  const per6 = Period("6th", "2:06", "2:58");
+  const per7 = Period("7th", "3:03", "3:55");
    //create an array "schedule" that contains 7 periods
   const schedule = [per1, per2, per3, per4, per5, per6, per7];
+  const day = currentDay();
     return(
-    <View style = {background}>
+    <ScrollView style = {styles.background}>
+    
+        <View style = {styles.headerBox}>
+          <Text style = {styles.headerText}>
+            Monarch Highschool Schedule
+          </Text>
+        </View>
+
+        <View style = {styles.dateBox}>
+          <Text style = {styles.dayText}>
+            {day}
+          </Text>
+        </View>
+
     {schedule.map(per =>{
         return(
           //assign PeriodBox values
           <PeriodBox 
+          isPeriod = {per.currentPeriod}
           period = {per.periodNumber} 
           start = {per.startTime} 
-          end = {per.endTime}  
-            
+          end = {per.endTime}       
           /> 
         )})}
-    </View>
+ 
+ 
+    </ScrollView>
+
+
     )   
-    
+
 
 }
 
-const background = {
-  backgroundColor: '#333333',
-  paddingTop: 10,
-};
+const styles = StyleSheet.create({ 
+  background: {
+    backgroundColor: '#333333',
+    paddingTop: 10,
+},
+headerBox: {
+  width: 370,
+  height: 50,
+  backgroundColor: '#DCCD98',
+  justifyContent: 'center',
+  top: 50,
+  borderRadius: 15,
+  left: 10,
+  right: 10,
+},
+headerText: {
+  fontFamily: 'Arial',
+  fontSize: 20, 
+  textAlign: 'center',
+},
+dateBox: {
+  top: 60,
+},
+dayText: {
+  color: '#FFFFFF',
+  fontFamily: 'Arial',
+  fontSize: 25,
+  fontWeight: '400',
+  textAlign: 'center',
+}
+})
