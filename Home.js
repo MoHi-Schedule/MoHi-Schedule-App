d88aaa87014d9a70e28d345c9a48292ebabe96a8
socho1 <joshmyeongkim@gmail.com>
2023-11-17 22:15:15 -0700

    added comments, more use of schedule param

--- Home.js
+++ Home.js
@@ -1,4 +1,4 @@
-import {PeriodBox, currentDay, getSchedule, isCurrentPeriod} from './Data'
+import {PeriodBox, currentDay, isCurrentPeriod} from './Data'
 import {styles} from './components/stylesheets'
 import {ScrollView, Text, View ,Image, Pressable} from 'react-native';
 import {useNavigation} from '@react-navigation/native'
@@ -12,28 +12,31 @@ export const Home = (props) => {
     <ScrollView style = {styles.background}>
         <View style = {styles.image}>
 
+    {/*adds a button to navigate to TimeLeft screen*/}
           <Pressable onPress={() => navigation.navigate('TimeLeft', {name: 'TimeLeft'})}>
            <Image style = {styles.image} source={require("./assets/info.png")}  />
           </Pressable>
 
+    {/*display for the header box and header text at the top of the screen*/}
         </View>
         <View style = {styles.headerBox}>
           <Text style = {styles.headerText}>
             Monarch High School Schedule
           </Text>
         </View>
-
+    {/*displays the current day under the header box*/}
         <View style = {styles.dateBox}>
           <Text style = {styles.dayText}>
             {day}
           </Text>
         </View>
 
+    {/*maps through the schedule and creates a view for each period using PeriodBox component*/}
     {schedule.map(per =>{
         return(
           //assign PeriodBox values
           <PeriodBox 
-          currentPeriod = {isCurrentPeriod(per.periodNumber)}
+          currentPeriod = {isCurrentPeriod(per.periodNumber, schedule)}
           periodNumber = {per.periodNumber} 
           startHour = {per.startHour} 
           startMin = {per.startMin}
--- TimeLeft.js
+++ TimeLeft.js
@@ -8,36 +8,35 @@ import {useNavigation} from '@react-navigation/native'
 export const TimeLeft = (props) => {
   const navigation = useNavigation();
   let schedule = props.schedule;
-  const currentPeriod = currentPeriodNumber();
+  const currentPeriod = currentPeriodNumber(schedule);
   //checks if there is a current period, if there is calculate how much time is remaining and show that in a view
-  if(currentPeriod != null)
-    {
+  if(currentPeriod != null){
       const hours = new Date().getHours() % 12; 
       const min = new Date().getMinutes(); 
       const endTime = schedule[4][currentPeriod-1] * 60 + schedule[5][currentPeriod-1]
-      var currentTime = (hours * 60) + min;
-      
-return (
-  <ScrollView style = {styles.background}>
-  
-    <Text style = {styles.headerText}> The Current Period is {currentPeriod} </Text>
-    <Text style = {styles.headerText}> {endTime - currentTime} Minutes Left</Text>
+      var currentTime = (hours * 60) + min;  
+      //screen if there is a current period
+      return (
+        <ScrollView style = {styles.background}>
+          {/*displays current period and how much time left in current period*/}
+          <Text style = {styles.headerText}> The Current Period is {currentPeriod} </Text>
+          <Text style = {styles.headerText}> {endTime - currentTime} Minutes Left</Text>
+          {/*button to navigate back to homescreen*/}
+          <Pressable onPress={() => navigation.navigate('Home', {name: 'Home'})}>
+            <Image style = {styles.image} source={require("./assets/info.png")}  />
+          </Pressable>
+        </ScrollView>
+        )}
 
-    <Pressable onPress={() => navigation.navigate('Home', {name: 'Home'})}>
-      <Image style = {styles.image} source={require("./assets/info.png")}  />
-    </Pressable>
-  </ScrollView>
-)}
-//if there isn't a current period return a different view
-  else
-  {
-    return(  
-    <ScrollView style = {styles.background}>
-      <Text style = {styles.headerText}> Not currently school hours!</Text>
-      <Pressable onPress={() => props.navigate('Home', {name: 'Home'})}>
-        <Image style = {styles.image} source={require("./assets/info.png")}  />
-      </Pressable>
-    </ScrollView>
-    )
-  }
-}
+  //if there isn't a current period return a different view
+  else{
+      return(  
+        <ScrollView style = {styles.background}>
+          <Text style = {styles.headerText}> Not currently school hours!</Text>
+          {/*button to navigate back to homescreen*/}
+          <Pressable onPress={() => props.navigate('Home', {name: 'Home'})}>
+            <Image style = {styles.image} source={require("./assets/info.png")}  />
+          </Pressable>
+        </ScrollView>
+        )}
+      }
